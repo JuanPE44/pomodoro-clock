d73fbbebbff4d0063f7de83bbda4cf535a13e04e
JuanpeDeveloper <juantandil123@gmail.com>
2025-02-28 00:14:05 -0300

    focussesions agregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { FocusSessions } from "./components/FocusSessions";
 import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <FocusSessions />
     </div>
   );
 }
