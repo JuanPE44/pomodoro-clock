5b2f29105ce0055e858aaffa26831ba0fe5d7918
JuanpeDeveloper <juantandil123@gmail.com>
2025-03-06 13:48:38 -0300

    color primario agregado

--- src/components/focusSessions/pomodoro/PomoSettings.tsx
+++ src/components/focusSessions/pomodoro/PomoSettings.tsx
@@ -1,49 +1,57 @@
-import { IconArrowDown } from "../../../icons/IconArrowDown"
-import { IconArrowUp } from "../../../icons/IconArrowUp"
+import { IconArrowDown } from "../../../icons/IconArrowDown";
+import { IconArrowUp } from "../../../icons/IconArrowUp";
 
-export function PomoSettings({time, modifyTime,startTimer}: {time: number, modifyTime: (operator: string) => void, startTimer: () => void}) {
-    return (
-        <>
-            <div className="h-full flex flex-col justify-between items-center relative overflow-hidden">
-          <div className="w-[90%] h-[30%] flex flex-col justify-center items-center m-5">
-            <h1 className="h-1/2 text-xl text-white font-bold flex items-center mb-2">
-              Preparate Para concentrarte
-            </h1>
-            <p className="h-1/2 text-center text-sm text-neutral-200">
-              We'll turn off notifications and app alerts during each session.
-              For longer sessions, we'll add a short break so you can recharge.
-            </p>
-          </div>
+export function PomoSettings({
+  time,
+  modifyTime,
+  startTimer,
+}: {
+  time: number;
+  modifyTime: (operator: string) => void;
+  startTimer: () => void;
+}) {
+  return (
+    <>
+      <div className="h-full flex flex-col justify-between items-center relative overflow-hidden">
+        <div className="w-[90%] h-[30%] flex flex-col justify-center items-center m-5">
+          <h1 className="h-1/2 text-xl text-white font-bold flex items-center mb-2">
+            Preparate Para concentrarte
+          </h1>
+          <p className="h-1/2 text-center text-sm text-neutral-200">
+            We'll turn off notifications and app alerts during each session. For
+            longer sessions, we'll add a short break so you can recharge.
+          </p>
+        </div>
 
-          <div className="w-[50%] h-[22%] m-5 bg-[#242323] flex flex-row justify-center items-center shadow-sm rounded-[3px] border-b border-white hover:brightness-125 hover:rounded-[5px] overflow-hidden">
-            <div className="w-[70%] h-full flex text-white items-center justify-center flex-col hover:bg-[#5c5c5c] hover:opacity-40">
-              <div className="text-xl">{time}</div>
-              <div className="text-xs">min</div>
-            </div>
-            <div className="w-[30%] h-full flex flex-col items-center justify-center">
-              <button
-                onClick={() => modifyTime("+")}
-                className="w-full h-1/2 text-white border-l border-b border-[#5c5c5c] hover:bg-[#5c5c5c] hover:opacity-40 flex items-center justify-center"
-              >
-                <IconArrowUp />
-              </button>
-              <button
-                onClick={() => modifyTime("-")}
-                className="w-full h-1/2 text-white border-l border-[#5c5c5c] hover:bg-[#5c5c5c] hover:opacity-40 flex items-center justify-center"
-              >
-                <IconArrowDown />
-              </button>
-            </div>
+        <div className="w-[50%] h-[22%] m-5 bg-[#242323] flex flex-row justify-center items-center shadow-sm rounded-[3px] border-b border-white hover:brightness-125 hover:rounded-[5px] overflow-hidden">
+          <div className="w-[70%] h-full flex text-white items-center justify-center flex-col hover:bg-[#5c5c5c] hover:opacity-40">
+            <div className="text-xl">{time}</div>
+            <div className="text-xs">min</div>
           </div>
-          <div className="w-full p-10 flex justify-center items-center">
+          <div className="w-[30%] h-full flex flex-col items-center justify-center">
             <button
-              onClick={startTimer}
-              className="w-full h-full cursor-pointer bg-neutral-100 text-black text-sm rounded-sm py-2 hover:opacity-60"
+              onClick={() => modifyTime("+")}
+              className="w-full h-1/2 text-white border-l border-b border-[#5c5c5c] hover:bg-[#5c5c5c] hover:opacity-40 flex items-center justify-center"
             >
-              ▶ Iniciar Sesion de concentracion
+              <IconArrowUp />
+            </button>
+            <button
+              onClick={() => modifyTime("-")}
+              className="w-full h-1/2 text-white border-l border-[#5c5c5c] hover:bg-[#5c5c5c] hover:opacity-40 flex items-center justify-center"
+            >
+              <IconArrowDown />
             </button>
           </div>
         </div>
-        </>
-    )
+        <div className="w-full p-10 flex justify-center items-center">
+          <button
+            onClick={startTimer}
+            className=" cursor-pointer bg-primary text-black text-sm rounded-sm py-2 px-3 hover:opacity-60"
+          >
+            ▶ Start focus session
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -4,11 +4,14 @@ export function NavBar() {
   return (
     <header className="bg-neutral-900 h-creen pt-10 px-1">
       <ul>
-        <li className="flex items-center gap-2 pr-18 py-1 bg-neutral-800 rounded-sm">
-          <div className="h-4 w-1 bg-task"></div>
-          <IconFocus />
-          <a href="#" className="text-white">
-            Focus sessions
+        <li>
+          <a
+            href="/"
+            className="flex items-center gap-2 pr-18 py-1 bg-neutral-800 rounded-sm"
+          >
+            <div className="h-4 w-1 bg-primary"></div>
+            <IconFocus />
+            <span className="text-white hidden sm:block">Focus sessions</span>
           </a>
         </li>
       </ul>
